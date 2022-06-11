ae9347ceee9005d6bbc042349ef6fce8cdedb233
nurulh0291 <nurulh0291@gmail.com>
2022-06-11 09:24:23 +0700

    Login Page
    Add login page

---
+++ application/views/login.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+<!-- <style >
+	a#signup{
+		text-align: center;
+	}
+</style> -->
+</head>
+<body>
+    <div class="container">
+      <div class="row justify-content-center mt-5">
+        <div class="col-md-4">
+          <div class="card">
+            <div class="card-header bg-transparent mb-0"><h5 class="text-center">Please <span class="font-weight-bold text-primary">LOGIN</span></h5></div>
+            <div class="card-body">
+              <form action="">
+                <div class="form-group">
+                  <input type="text" name="" class="form-control" placeholder="Username">
+                </div>
+                <div class="form-group">
+                  <input type="text" name="" class="form-control" placeholder="Password">
+                </div>
+                <!-- <div class="form-group custom-control custom-checkbox">
+                  <input type="checkbox" class="custom-control-input" id="customControlAutosizing">
+                  <label class="custom-control-label" for="customControlAutosizing">Remember me</label>
+                </div> -->
+                <div class="form-group">
+                  <input type="submit" name="" value="Login" class="btn btn-primary btn-block">
+                </div>
+                <hr>
+            <!-- <p>Don't have an account!</p>  -->
+            	<a href="#" id="signup">Sign up New Account</a></p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </body>
+
+ <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+</html>
